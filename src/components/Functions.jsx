2e6829f831ecdbcacbe604d765110cf27b9e37cf
razor <wesleyrsnow@gmail.com>
2022-11-13 21:39:20 -0300

    Commit 04

--- src/components/Functions.jsx
+++
@@ -1,6 +0,0 @@
-export const PokemonModal = (uid, arr, SelectedPoke, ShowModal) => {
-  let tempPoke = arr.filter((poke) => (poke.id == uid ? poke : null));
-
-  SelectedPoke(tempPoke[0]);
-  ShowModal(true);
-};
